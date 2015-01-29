cc171a11f7031342a0e9a2a45afc1f654d07c89b
Dima Osadchy <dima.osadchy@gmail.com>
2015-01-29 14:08:55 +0200

    convert app to blueprints

--- app.py
+++ app.py
@@ -1,34 +1,36 @@
-from flask import Flask, render_template, redirect, url_for, request, session, flash
-
+from flask import Flask, flash, redirect, session, url_for, render_template, request
 from flask.ext.sqlalchemy import SQLAlchemy 
 from functools import wraps
-
-app = Flask(__name__)
-#import different config for diffferent env
 import os
-app.config.from_object(os.environ['APP_SETTINGS'])
 
-# create the sqlalchemy object
+### config ###
+app = Flask(__name__)
+app.config.from_object(os.environ['APP_SETTINGS'])
 db = SQLAlchemy(app)
 
 from models import *
+from project.users.views import users_blueprint
 
-def login_required(f):
-	@wraps(f)
+
+app.register_blueprint(users_blueprint)
+
+
+### helper function ###
+def login_required(test):
+	@wraps(test)
 	def wrap(*args, **kwargs):
 		if 'logged_in' in session:
-			return f(*args, **kwargs)
+			return test(*args, **kwargs)
 		else:
 			flash('You need to login first.')
-			return redirect(url_for('login'))
+			return redirect(url_for('users.login'))
 	return wrap
 
+### routes ###
 
 @app.route('/', methods=['GET', 'POST'])
 @login_required
 def home():
-	
-	posts = []
 
 	try:
 		if request.method == 'POST':
@@ -38,37 +40,16 @@ def home():
 				flash(request.form['post'])
 			 	db.session.add(BlogPost(request.form['title'], request.form['post']))
 			 	db.session.commit()
-
-		posts = db.session.query(BlogPost).all()
 	except:
 		flash("Some error in db")
 
+ 	posts = db.session.query(BlogPost).all()
 	return render_template("index.html", posts=posts)
 
 @app.route('/welcome')
 def welcome():
 	return render_template("welcome.html")
 
-@app.route("/login", methods=['GET', 'POST'])
-def login():
-	error = None
-	if request.method == 'POST':
-		if request.form['username'] != 'admin' or request.form['password'] != 'admin':
-			error = 'Invalid creantials. Please try again'
-		else:
-			session['logged_in'] = True
-			flash('You were just logged in!')
-			return redirect(url_for('home'))
-	return render_template("login.html", error=error)
-@app.route('/logout')
-@login_required
-def logout():
-	session.pop('logged_in', None)
-	flash('You were just logged out!')
-	return redirect(url_for('welcome'))
-
-
+### run server ###
 if __name__ == '__main__':
 	app.run()
-
-
