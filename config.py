aaa2409cd3484ece3d5895bc947af968b62868a7
Dima Osadchy <dima.osadchy@gmail.com>
2015-01-29 11:23:21 +0200

    added migrations

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ class BaseConfig(object):
 	DEBUG = False
 	SECRET_KEY = '\x15\xc8A\xdc\x17\xea0\x84\xd6\xc1\xab:\xf4\xd7e\x94\x1b\x90\xfb[\x9e5\x1a\xd7'
 	SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
-
+	print SQLALCHEMY_DATABASE_URI
 class DevelopmentConfig(BaseConfig):
 	DEBUG = True
 
--- models.py
+++ models.py
@@ -1,5 +1,8 @@
 from app import db
 
+from sqlalchemy import ForeignKey
+from sqlalchemy.orm import relationship
+
 
 class BlogPost(db.Model):
 
@@ -8,11 +11,33 @@ class BlogPost(db.Model):
 	id = db.Column(db.Integer, primary_key=True)
 	title = db.Column(db.String, nullable=False)
 	description = db.Column(db.String, nullable=False)
+	author_id = db.Column(db.Integer, ForeignKey('users.id'))
 
 	def __init__(self, title, description):
 		self.title = title
 		self.description = description
 
 	def __repr__(self):
-		return '{} - {}'.format(self.title, self.description)
+		return '{} - {}'.format(self.title, self.description).uncode('utf-8')
+
+class User(db.Model):
+
+	__tablename__ = "users"
+
+	id = db.Column(db.Integer, primary_key=True)
+	name = db.Column(db.String, nullable=False)
+	email = db.Column(db.String, nullable=False)
+	password = db.Column(db.String, nullable=False)
+	post = relationship("BlogPost", backref="author")
+
+	def __init__(self, name, email, password):
+		self.name = name
+		self.email = email
+		self.password = password
+
+	def __repr__(self):
+		return '{} - {}'.format(self.name, self.email)
+
+
+
 
