99d4f235416edf47a697e1606d8d35ad885984cd
Dima Osadchy <dima.osadchy@gmail.com>
2015-01-29 10:14:47 +0200

    change secret key

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ import os
 
 class BaseConfig(object):
 	DEBUG = False
-	SECRET_KEY = os.urandom(24)
+	SECRET_KEY = '\x15\xc8A\xdc\x17\xea0\x84\xd6\xc1\xab:\xf4\xd7e\x94\x1b\x90\xfb[\x9e5\x1a\xd7'
 	SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
 
 class DevelopmentConfig(BaseConfig):
